5fe1f4980110c360daff57e5ad1052804c7c4298
benzkittisak <benzkittisak@stv.ac.th>
2022-03-24 16:32:13 +0700

    Initial commit

---
+++ app/src/main/java/kpdev/enterprise/floatingactionbutton/MainActivity.java
@@ -0,0 +1,33 @@
+package kpdev.enterprise.floatingactionbutton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    TextView textView;
+    FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+        fab = (FloatingActionButton) findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener( this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        textView.setText("Floating Action Button Mail Clicked !");
+        Intent intent = new Intent( this, RegistorActivity.class);
+        startActivity( intent );
+    }
+}
